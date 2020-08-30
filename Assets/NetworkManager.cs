d1f1e4b5749ace432adef3a11bfa4b935007fd73
JaesunHan <jaesunhan93@gmail.com>
2020-08-30 19:17:22 +0900

    네트워크 매니저 작업중

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public Text StatusText;
+    public InputField roomInput, NickNameInput;
+
+    private void Awake() => Screen.SetResolution(960, 540, false);
+
+    void Update() => StatusText.text = PhotonNetwork.NetworkClientState.ToString();
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        print("서버 접속 완료");
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+    }
+
+    public void Disconnect() => PhotonNetwork.Disconnect();
+
+    public override void OnDisconnected(DisconnectCause cause) => print("연결 끊김");
+
+    public void JoinLobby() => PhotonNetwork.JoinLobby();
+
+    public override void OnJoinedLobby() => print("로비 접속 완료");
+
+
+    public void CreateRoom() => PhotonNetwork.CreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 2 });
+
+    public void JoinRoom() => PhotonNetwork.JoinRoom(roomInput.text);
+
+    public void JoinOrCreateRoom() => PhotonNetwork.JoinOrCreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 2 }, null);
+
+    public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
+
+    public void LeaveRoom() => PhotonNetwork.LeaveRoom();
+
+    public override void OnCreatedRoom() => print("방 만들기 완료");
+
+    public override void OnJoinedRoom() => print("방참가 완료");
+
+
+}
